d0bf9613dfd75088b282dd188a7537a21a46a94d
Tushar <tushar21it@student.mes.ac.in>
2023-03-19 12:54:05 +0530

    nine commit

--- pages/nav-link.js
+++
@@ -1,9 +0,0 @@
-// "use client";
-// import Link from "next/link";
-// import { useSelectedLayoutSegment } from "next/navigation";
-// export default function NavLink({href,children}){
-//     let segment =useSelectedLayoutSegment();
-//     let active = href ===`/${segment}`
-//     console.log({href,active});
-//     return <Link className={active ? "border-b-4 border-green":""} href={href}>{children}</Link>
-// }
